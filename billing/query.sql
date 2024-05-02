e81cf21b3e7bd73bc90dd255115c6a4aa106f943
vgamarra2021 <vgamarrav@uni.pe>
2024-05-02 12:50:53 -0500

    comment sql in billing

--- billing/query.sql
+++ billing/query.sql
@@ -3,47 +3,66 @@ select
 from
     (
         select
+            --ExternalSystemOrderId
             dispatch_order.external_system_order_id as orden_compra,
+            --Courier
             hd_delivery_dispatch_raw_prod_spec.courier.name as sistema,
+            --ExternalDriverId
             hd_delivery_dispatch_raw_prod.dispatch_order.external_driver_id as codigo_conductor,
+            --Codigo País
+            hd_delivery_dispatch_raw_prod_spec.country.code as codigo_pais,
+            --Courier
             case
-                when hd_delivery_dispatch_raw_prod_spec.courier.name = 'Beetrack' then tags1.value
-                when hd_delivery_dispatch_raw_prod_spec.courier.name = 'Home Delivery Chile' then provider.name
-                else hd_delivery_dispatch_raw_prod_spec.courier.name
+                when sistema = 'Beetrack' then tags1.value --Tags1 Table
+                when sistema = 'Home Delivery Chile' then provider.name --Provider Table
+                else sistema --Courier
             end as proveedor,
+            --LicencePlate
             case
-                when hd_delivery_dispatch_raw_prod_spec.courier.name = 'Beetrack' then tags2.value
-                when hd_delivery_dispatch_raw_prod_spec.courier.name = 'Home Delivery Chile' then hd_delivery_tms_raw_prod_spec.vehicle.licence_plate
-                else hd_delivery_dispatch_raw_prod_spec.courier.name
+                when sistema = 'Beetrack' then tags2.value --Tags2 Table
+                when sistema = 'Home Delivery Chile' then hd_delivery_tms_raw_prod_spec.vehicle.licence_plate --Vehicle Licence Plate
+                else sistema --Courier
             end as patente,
+            --driverName
             case
-                when hd_delivery_dispatch_raw_prod_spec.courier.name = 'Beetrack' then hd_delivery_dispatch_raw_prod.vehicle_driver.full_name
-                else hd_delivery_tms_raw_prod_spec.driver.full_name
+                when sistema = 'Beetrack' then hd_delivery_dispatch_raw_prod.vehicle_driver.full_name --driverName from VehicleDriver
+                else hd_delivery_tms_raw_prod_spec.driver.full_name --driverName from driver
             end as nombre_driver,
+            --RetailChainName
             retail_chain.name as negocio,
+            --StoreCode
             known_source.external_id as codigo_tienda,
+            --StoreName
             known_source.name as nombre_tienda,
+            --EventDate from TrackingEvents BD
             tmp_te."event-date" :: timestamp as fecha_evento,
+            --CommitmentDate
             case
-                when hd_delivery_dispatch_raw_prod_spec.country.code = 'PE' then date(
+                when codigo_pais = 'PE' then date(
                     dispatch_order.eta_end_date :: timestamp AT TIME ZONE 'UTC-5'
                 )
-                when hd_delivery_dispatch_raw_prod_spec.country.code = 'CO' then date(
+                when codigo_pais = 'CO' then date(
                     dispatch_order.eta_end_date :: timestamp AT TIME ZONE 'UTC-5'
                 )
-                when hd_delivery_dispatch_raw_prod_spec.country.code = 'AR' then date(
+                when codigo_pais = 'AR' then date(
                     dispatch_order.eta_end_date :: timestamp AT TIME ZONE 'UTC-3'
                 )
-                when hd_delivery_dispatch_raw_prod_spec.country.code = 'BR' then date(
+                when codigo_pais = 'BR' then date(
                     dispatch_order.eta_end_date :: timestamp AT TIME ZONE 'UTC-3'
                 )
                 else date(dispatch_order.eta_end_date :: timestamp)
             end as fecha_compromiso,
+            --LastStatusDesc
             pack2.last_status_desc as nombre_estado,
+            --LastSubStatusDesc
             pack2.last_sub_status_desc as nombre_sub_estado,
+            --SubAdminZone2 Name
             saz2_destination.name as comuna_destino,
+            --DestinationLatitude
             destination.latitude as latitude,
+            --DestinationLogitude
             destination.longitude as longitude,
+            --OnTime
             case
                 when hd_delivery_dispatch_raw_prod_spec.country.code = 'PE'
                 and (
@@ -145,15 +164,18 @@ from
                 ) >= '09/03/2023' then 1
                 when hd_delivery_dispatch_raw_prod_spec.country.code = 'CL'
                 and (
-                    DATE_TRUNC('minute', tmp_te."event-date" :: timestamp) <= DATE_TRUNC('minute', dispatch_order.eta_end_date :: timestamp)
+                    DATE_TRUNC('minute', tmp_te."event-date" :: timestamp) <= DATE_TRUNC(
+                        'minute',
+                        dispatch_order.eta_end_date :: timestamp
+                    )
                 ) then 1
                 else 0
             end as on_time,
-            --tags_bultos.total::integer as total_bultos,
-            --tags_bultos.size as size,
+            --OrderKey
             dispatch_order.external_system_order_id || '|' || max(dispatch_order.updated_at :: timestamp) as order_key
         from
-            hd_delivery_dispatch_raw_prod.dispatch_order --Pack
+            --DispatchOrder
+            hd_delivery_dispatch_raw_prod.dispatch_order
             left join (
                 select
                     *
@@ -182,7 +204,7 @@ from
                         select
                             tmp00.pack_key
                         from
-(
+                            (
                                 select
                                     pack.tracking,
                                     pack.tracking || '|' || max(pack.updated_at :: timestamp) as pack_key
@@ -192,18 +214,18 @@ from
                                     1
                             ) tmp00
                     )
-            ) as pack2 on pack2.dispatch_order_id = dispatch_order.id --Destination
-            left join hd_delivery_dispatch_raw_prod.destination on destination.id = dispatch_order.destination_id
-            left join hd_delivery_dispatch_raw_prod_spec.sub_admin_zone_1 saz1_destination on saz1_destination.id = destination.sub_admin_zone_1_id
-            left join hd_delivery_dispatch_raw_prod_spec.sub_admin_zone_2 saz2_destination on saz2_destination.id = destination.sub_admin_zone_2_id --Known Source
-            left join hd_delivery_dispatch_raw_prod_spec.known_source on known_source.id = dispatch_order.owner_known_source_id -- Retail chain
-            left join hd_delivery_dispatch_raw_prod_spec.retail_chain on hd_delivery_dispatch_raw_prod_spec.retail_chain.id = hd_delivery_dispatch_raw_prod.dispatch_order.retail_chain_id -- Country
-            left join hd_delivery_dispatch_raw_prod_spec.country on hd_delivery_dispatch_raw_prod_spec.country.id = hd_delivery_dispatch_raw_prod_spec.retail_chain.country_id -- Dispatch Type
-            left join hd_delivery_dispatch_raw_prod_spec.dispatch_type on hd_delivery_dispatch_raw_prod.dispatch_order.dispatch_type_id = hd_delivery_dispatch_raw_prod_spec.dispatch_type.id -- Courier
-            left join hd_delivery_dispatch_raw_prod_spec.courier on hd_delivery_dispatch_raw_prod_spec.courier.id = hd_delivery_dispatch_raw_prod.dispatch_order.courier_id -- Driver TMS
-            left join hd_delivery_tms_raw_prod_spec.driver on hd_delivery_tms_raw_prod_spec.driver.id = hd_delivery_dispatch_raw_prod.dispatch_order.external_driver_id -- Vehicle
-            left join hd_delivery_tms_raw_prod_spec.vehicle on hd_delivery_tms_raw_prod_spec.vehicle.id = hd_delivery_tms_raw_prod_spec.driver.vehicle_id -- Provider 
-            left join hd_delivery_dispatch_raw_prod_spec.provider on hd_delivery_tms_raw_prod_spec.driver.provider_id = hd_delivery_dispatch_raw_prod_spec.provider.id -- Event Date
+            ) as pack2 on pack2.dispatch_order_id = dispatch_order.id --TODO
+            left join hd_delivery_dispatch_raw_prod.destination on destination.id = dispatch_order.destination_id --Destination
+            left join hd_delivery_dispatch_raw_prod_spec.sub_admin_zone_1 saz1_destination on saz1_destination.id = destination.sub_admin_zone_1_id --SubAdminZone1 Destination
+            left join hd_delivery_dispatch_raw_prod_spec.sub_admin_zone_2 saz2_destination on saz2_destination.id = destination.sub_admin_zone_2_id --SubAdminZone2 Destination
+            left join hd_delivery_dispatch_raw_prod_spec.known_source on known_source.id = dispatch_order.owner_known_source_id --Known Source
+            left join hd_delivery_dispatch_raw_prod_spec.retail_chain on hd_delivery_dispatch_raw_prod_spec.retail_chain.id = hd_delivery_dispatch_raw_prod.dispatch_order.retail_chain_id --Retail chain
+            left join hd_delivery_dispatch_raw_prod_spec.country on hd_delivery_dispatch_raw_prod_spec.country.id = hd_delivery_dispatch_raw_prod_spec.retail_chain.country_id --Country
+            left join hd_delivery_dispatch_raw_prod_spec.dispatch_type on hd_delivery_dispatch_raw_prod.dispatch_order.dispatch_type_id = hd_delivery_dispatch_raw_prod_spec.dispatch_type.id --Dispatch Type
+            left join hd_delivery_dispatch_raw_prod_spec.courier on hd_delivery_dispatch_raw_prod_spec.courier.id = hd_delivery_dispatch_raw_prod.dispatch_order.courier_id --Courier
+            left join hd_delivery_tms_raw_prod_spec.driver on hd_delivery_tms_raw_prod_spec.driver.id = hd_delivery_dispatch_raw_prod.dispatch_order.external_driver_id --Driver TMS
+            left join hd_delivery_tms_raw_prod_spec.vehicle on hd_delivery_tms_raw_prod_spec.vehicle.id = hd_delivery_tms_raw_prod_spec.driver.vehicle_id --Vehicle TMS
+            left join hd_delivery_dispatch_raw_prod_spec.provider on hd_delivery_tms_raw_prod_spec.driver.provider_id = hd_delivery_dispatch_raw_prod_spec.provider.id --Provider from TMS
             left join (
                 select
                     te.external_system_order_id,
@@ -221,36 +243,7 @@ from
                         group by
                             te0.external_system_order_id
                     )
-            ) as tmp_te on dispatch_order.external_system_order_id = tmp_te.external_system_order_id -- Bultos
-            --left join 
-            --(select *,
-            --case
-            --when tmp_tags.total>0 and tmp_tags.total<=5 then 'MP'
-            --when tmp_tags.total>5 and tmp_tags.total<=12 then 'P'
-            --when tmp_tags.total>12 and tmp_tags.total<=20 then 'M'
-            --when tmp_tags.total>20 then 'G'
-            --else ''
-            --end 
-            --as size
-            --from (
-            --select
-            --id,
-            --created_at,
-            --updated_at,
-            --name,
-            --value,
-            --dispatch_order_id,
-            --REGEXP_COUNT(value, '"quantity":1')+REGEXP_COUNT(value, '"quantity":2')*2+REGEXP_COUNT(value, '"quantity":3')*3+REGEXP_COUNT(value, '"quantity":4')*4+REGEXP_COUNT(value, '"quantity":5')*5+
-            --REGEXP_COUNT(value, '"quantity":6')*6+REGEXP_COUNT(value, '"quantity":7')*7+REGEXP_COUNT(value, '"quantity":8')*8+REGEXP_COUNT(value, '"quantity":9')*9+REGEXP_COUNT(value, '"quantity":10')*10+
-            --REGEXP_COUNT(value, '"quantity":11')*11+REGEXP_COUNT(value, '"quantity":12')*12+REGEXP_COUNT(value, '"quantity":13')*13+REGEXP_COUNT(value, '"quantity":14')*14+REGEXP_COUNT(value, '"quantity":15')*15
-            --as total
-            --from
-            --hd_delivery_dispatch_raw_prod."tag"
-            --where 1=1
-            --and hd_delivery_dispatch_raw_prod."tag".name='items'
-            --) as tmp_tags
-            --) as tags_bultos
-            --on dispatch_order.id = tags_bultos.dispatch_order_id
+            ) as tmp_te on dispatch_order.external_system_order_id = tmp_te.external_system_order_id -- Tracking Events, LastStatus
             left join (
                 select
                     dispatch_order_id,
@@ -269,7 +262,7 @@ from
                         select
                             aux0.maxid
                         from
-(
+                            (
                                 select
                                     dispatch_order_id,
                                     name,
@@ -286,7 +279,7 @@ from
                                     name
                             ) as aux0
                     )
-            ) as tags1 on dispatch_order.id = tags1.dispatch_order_id
+            ) as tags1 on dispatch_order.id = tags1.dispatch_order_id --Tags
             left join (
                 select
                     dispatch_order_id,
@@ -305,7 +298,7 @@ from
                         select
                             aux1.maxid
                         from
-(
+                            (
                                 select
                                     dispatch_order_id,
                                     name,
@@ -322,13 +315,13 @@ from
                                     name
                             ) as aux1
                     )
-            ) as tags2 on dispatch_order.id = tags2.dispatch_order_id
-            left join hd_delivery_dispatch_raw_prod.vehicle_driver on hd_delivery_dispatch_raw_prod.vehicle_driver.id = hd_delivery_dispatch_raw_prod.dispatch_order.vehicle_driver_id
+            ) as tags2 on dispatch_order.id = tags2.dispatch_order_id --Tags2
+            left join hd_delivery_dispatch_raw_prod.vehicle_driver on hd_delivery_dispatch_raw_prod.vehicle_driver.id = hd_delivery_dispatch_raw_prod.dispatch_order.vehicle_driver_id --VehicleDriver
         where
             1 = 1
-            and dispatch_order.retail_chain_id in (1, 4, 9)
-            and pack2.last_status_code not in ('10', '1002')
-            and pack2.last_status_code in ('09', '02', '04', '03')
+            and dispatch_order.retail_chain_id in (1, 4, 9) --RetailChainId
+            and pack2.last_status_code not in ('10', '1002') --LastStatusCode
+            and pack2.last_status_code in ('09', '02', '04', '03') --LastStatusCode
         group by
             1,
             2,
@@ -346,7 +339,8 @@ from
             14,
             15,
             16,
-            17 --[[and {{created_at}}]]
+            17,
+            18 --[[and {{created_at}}]]
             --[[and {{update_at}}]]
             --[[and {{nombre_tienda}}]]
             --[[and {{codigo_tienda}}]]
@@ -364,7 +358,7 @@ where
         select
             tmp000.order_key
         from
-(
+            (
                 select
                     dispatch_order.external_system_order_id,
                     dispatch_order.external_system_order_id || '|' || max(dispatch_order.updated_at :: timestamp) as order_key
@@ -374,9 +368,10 @@ where
                     1
             ) as tmp000
     )
+    and tmp.orden_compra = 'v121932843jmch-01'
     and tmp.negocio = '{negocio}'
     and tmp.proveedor <> 'None'
-    and tmp.nombre_driver <> 'None' --and tmp.fecha_evento >= '2023-12-01 0:00:00'
+    and tmp.nombre_driver <> 'None'
     and tmp.fecha_compromiso >= '{fecha_compromiso_inicial}'
     and tmp.fecha_compromiso <= '{fecha_compromiso_final}' --[[and  tmp.fecha_evento>= ({{updated_at1}}::date || ' 0:00:00')::timestamp]]
     --[[and  tmp.fecha_evento<= ({{updated_at2}}::date || ' 23:59:59')::timestamp]]
